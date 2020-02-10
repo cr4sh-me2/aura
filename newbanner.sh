294e6c041795a0e422f79068c6ba8c549a121b8d
iphunt3r <54245632+rkhunt3r@users.noreply.github.com>
2020-02-10 15:29:34 +0100

    Add files via upload

---
+++ newbanner.sh
@@ -0,0 +1,21 @@
+printf "
+
+  aaaaaaaaaaaaa  uuuuuu    uuuuuu rrrrr   rrrrrrrrr   aaaaaaaaaaaaa   
+  a::::::::::::a u::::u    u::::u r::::rrr:::::::::r  a::::::::::::a  
+  aaaaaaaaa:::::au::::u    u::::u r:::::::::::::::::r aaaaaaaaa:::::a 
+           a::::au::::u    u::::u rr::::::rrrrr::::::r         a::::a 
+    aaaaaaa:::::au::::u    u::::u  r:::::r     r:::::r  aaaaaaa:::::a 
+  aa::::::::::::au::::u    u::::u  r:::::r     rrrrrrraa::::::::::::a 
+ a::::aaaa::::::au::::u    u::::u  r:::::r           a::::aaaa::::::a 
+a::::a    a:::::au:::::uuuu:::::u  r:::::r          a::::a    a:::::a 
+a::::a    a:::::au:::::::::::::::uur:::::r          a::::a    a:::::a 
+a:::::aaaa::::::a u:::::::::::::::ur:::::r          a:::::aaaa::::::a 
+ a::::::::::aa:::a uu::::::::uu:::ur:::::r           a::::::::::aa:::a
+  aaaaaaaaaa  aaaa   uuuuuuuu  uuuurrrrrrr            aaaaaaaaaa  aaaa
+"
+                                                                      
+                                                                      
+                                                                      
+                                                                      
+                                                                      
+                                                                      
