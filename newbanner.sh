59b8320953b0d1cc33cd34849e6d957ca654a97e
iphunt3r <54245632+rkhunt3r@users.noreply.github.com>
2020-02-10 17:54:59 +0100

    Add files via upload

--- newbanner.sh
+++ newbanner.sh
@@ -1,17 +1,17 @@
 printf "
 
-  aaaaaaaaaaaaa  uuuuuu    uuuuuu rrrrr   rrrrrrrrr   aaaaaaaaaaaaa   
-  a::::::::::::a u::::u    u::::u r::::rrr:::::::::r  a::::::::::::a  
-  aaaaaaaaa:::::au::::u    u::::u r:::::::::::::::::r aaaaaaaaa:::::a 
-           a::::au::::u    u::::u rr::::::rrrrr::::::r         a::::a 
-    aaaaaaa:::::au::::u    u::::u  r:::::r     r:::::r  aaaaaaa:::::a 
-  aa::::::::::::au::::u    u::::u  r:::::r     rrrrrrraa::::::::::::a 
- a::::aaaa::::::au::::u    u::::u  r:::::r           a::::aaaa::::::a 
-a::::a    a:::::au:::::uuuu:::::u  r:::::r          a::::a    a:::::a 
-a::::a    a:::::au:::::::::::::::uur:::::r          a::::a    a:::::a 
-a:::::aaaa::::::a u:::::::::::::::ur:::::r          a:::::aaaa::::::a 
- a::::::::::aa:::a uu::::::::uu:::ur:::::r           a::::::::::aa:::a
-  aaaaaaaaaa  aaaa   uuuuuuuu  uuuurrrrrrr            aaaaaaaaaa  aaaa
+\e[38;5;196m  aaaaaaaaaaaaa  uuuuuu    uuuuuu rrrrr   rrrrrrrrr   aaaaaaaaaaaaa   
+\e[38;5;196m  a::::::::::::a u::::u    u::::u r::::rrr:::::::::r  a::::::::::::a  
+\e[38;5;196m  aaaaaaaaa:::::au::::u    u::::u r:::::::::::::::::r aaaaaaaaa:::::a 
+\e[38;5;82m           a::::au::::u    u::::u rr::::::rrrrr::::::r         a::::a 
+\e[38;5;82m    aaaaaaa:::::au::::u    u::::u  r:::::r     r:::::r  aaaaaaa:::::a 
+\e[38;5;82m  aa::::::::::::au::::u    u::::u  r:::::r     rrrrrrraa::::::::::::a 
+\e[38;5;226m a::::aaaa::::::au::::u    u::::u  r:::::r           a::::aaaa::::::a 
+\e[38;5;226ma::::a    a:::::au:::::uuuu:::::u  r:::::r          a::::a    a:::::a 
+\e[38;5;226ma::::a    a:::::au:::::::::::::::uur:::::r          a::::a    a:::::a 
+\e[38;5;4ma:::::aaaa::::::a u:::::::::::::::ur:::::r          a:::::aaaa::::::a 
+\e[38;5;4m a::::::::::aa:::a uu::::::::uu:::ur:::::r           a::::::::::aa:::a
+\e[38;5;4m  aaaaaaaaaa  aaaa   uuuuuuuu  uuuurrrrrrr            aaaaaaaaaa  aaaa
 "
                                                                       
                                                                       
