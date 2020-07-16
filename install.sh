539694523df0e8f78170b8155c2c1a637c69d74c
iphunt3r <54245632+rkhunt3r@users.noreply.github.com>
2020-07-16 22:12:48 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -12,6 +12,16 @@ banner()
 '
 }
 
+rootcheck()
+{
+if [[ $EUID -ne 0 ]]; then
+   echo "\n\e[0m[\e[91m!\e[0m] THIS SCRIPT MUST BE RUN AS ROOT! ABORTING...\n" 
+   exit 1
+fi
+}
+
+rootcheck
+
 info='\n\e[0m[\e[92mi\e[0m] INSTALLING TOOL...\n'
 
 banner
@@ -35,7 +45,7 @@ command -v aircrack-ng >/dev/null 2>&1 || { printf >&2 "\e[0m$info\n "; apt-get 
      command -v unzip >/dev/null 2>&1 || { printf >&2 "\e[0m$info\n "; apt-get install unzip -y; }
     command -v curl >/dev/null 2>&1 || { printf >&2 "\e[0m$info\n "; apt-get install curl -y; }
     command -v wget >/dev/null 2>&1 || { printf >&2 "\e[0m$info\n "; apt-get install wget -y; }
-
+    
 command -v lolcat >/dev/null 2>&1 || { printf >&2 "\e[0m$info\n "; 
 wget https://github.com/busyloop/lolcat/archive/master.zip
 unzip master.zip
